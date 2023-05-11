f8148b2f4ee5da8d7437076257f97307db6e0ca6
iT3E <26583558+iT3E@users.noreply.github.com>
2023-05-10 23:00:57 -0700

    feat: pve k3s vms

--- terraform/environments/development/pve/main.tf
+++ terraform/environments/development/pve/main.tf
@@ -1,1 +1,21 @@
-#test
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = ">= 2.9.10"
+    }
+  }
+}
+
+locals {
+  pve_vms = [
+    "sce-ubu01",
+    "sce-ubu02",
+    "sce-ubu03",
+  ]
+}
+
+module "pve_vm" {
+  for_each         = toset(local.pve_cms)
+  source           = "./modules/proxmox"
+}
