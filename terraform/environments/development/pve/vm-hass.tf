6ba99b16a27ea39e3e42103669c1cea611b720f7
iT3E <26583558+iT3E@users.noreply.github.com>
2023-05-13 10:42:49 -0700

    feat: vhardware changes

--- terraform/environments/development/pve/vm-hass.tf
+++ terraform/environments/development/pve/vm-hass.tf
@@ -11,7 +11,7 @@ module "pve_vm" {
   iso_image_location   = "ceph:iso/ubuntu20.1"
   ha_group             = "ha_group${index(local.pve_vms, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
   memory               = "8192"
-  cores                = "4"
+  cores                = "2"
   sockets              = "2"
   qemu_os              = "l26"
   agent                = 1
