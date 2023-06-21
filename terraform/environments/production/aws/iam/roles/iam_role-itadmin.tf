874cfb594d460bc136a1f267333464aa496c8387
iT3E <26583558+iT3E@users.noreply.github.com>
2023-06-21 15:43:57 -0700

    feat: itadmin role creation for sso and sops iam

--- terraform/environments/production/aws/iam/roles/iam_role-itadmin.tf
+++ terraform/environments/production/aws/iam/roles/iam_role-itadmin.tf
@@ -1,19 +1,4 @@
-module "iam_sops_role" {
+module "iam_itadmin_role" {
   source             = "modules/aws/iam/roles"
   name               = "itadmin_role"
-  assume_role_policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": "${module.iam_role.itadmin.role_arn}"
-        },
-        "Action": "sts:assume",
-        "Resource": "*"
-      }
-    ]
-  }
-POLICY
 }
