8be54a826969e183cbf8ec9010de7ebe122a2aa1
iT3E <26583558+iT3E@users.noreply.github.com>
2023-06-21 15:33:33 -0700

    feat: aws itadmin role and sops secrets

---
+++ terraform/environments/production/aws/iam/roles/iam_role-itadmin.tf
@@ -0,0 +1,19 @@
+module "iam_sops_role" {
+  source             = "modules/aws/iam/roles"
+  name               = "itadmin_role"
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "${module.iam_role.itadmin.role_arn}"
+        },
+        "Action": "sts:assume",
+        "Resource": "*"
+      }
+    ]
+  }
+POLICY
+}
