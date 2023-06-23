b9b88425caa22d1c4cf73295865a63cf8a8b97e2
iT3E <26583558+iT3E@users.noreply.github.com>
2023-06-23 14:02:42 -0700

    feat: tf restructure

--- terraform/environments/production/aws/main.tf
+++ terraform/environments/production/aws/main.tf
@@ -24,13 +24,84 @@ data "sops_file" "secrets" {
   source_file = "secrets.sops.yaml"
 }
 
-module "kms_sops" {
-  source = "./kms/"
+
+#####################################
+##                                 ##
+##        KMS Key - Sops           ##
+##                                 ##
+#####################################
+
+data "aws_iam_policy_document" "kms_key_policy" {
+  statement {
+    sid    = "EnableIAMUserPermissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [module.iam_role.iam_role_sops.role_arn]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
 }
 
-module "iam_role_itadmin" {
-  source = "./iam/roles"
+module "kms_sops" {
+  source          = "../../../modules/aws/kms"
+  key_description = "KMS Key for SOPS"
+  key_policy      = data.aws_iam_policy_document.kms_key_policy.json
 }
+
+
+########################
+##                    ##
+## IAM Role - itadmin ##
+##                    ##
+########################
+
+module "iam_role_itadmin" {
+  source             = "../../../modules/aws/iam/roles"
+  iam_role_name               = "itadmin_role"
+}
+
+########################
+##                    ##
+##  IAM Role - sops   ##
+##                    ##
+########################
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [module.iam_role.itadmin.role_arn]
+    }
+  }
+}
+
 module "iam_role_sops" {
-  source = "./iam/roles"
+  source             = "../../../modules/aws/iam/roles"
+  iam_role_name               = "sops_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+#####################################
+##                                 ##
+##  IAM Policy - KMS Access Sops   ##
+##                                 ##
+#####################################
+
+data "aws_iam_policy_document" "kms_access_policy" {
+  statement {
+    actions   = ["kms:Decrypt", "kms:Encrypt", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:DescribeKey"]
+    resources = [module.kms_sops.key_arn]
+  }
+}
+
+module "kms_access_policy" {
+  source          = "../../../modules/aws/iam/policies"
+  policy_name     = "kms_sops_key_policy"
+  policy_json     = data.aws_iam_policy_document.kms_access_policy.json
 }
