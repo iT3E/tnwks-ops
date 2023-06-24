5d6f734f49be39da767c53c888616407aae766d2
iT3E <26583558+iT3E@users.noreply.github.com>
2023-06-23 19:05:57 -0700

    adding sops pve secrets

--- terraform/environments/production/pve/main.tf
+++ terraform/environments/production/pve/main.tf
@@ -1,8 +1,25 @@
 terraform {
-  required_providers {
-    proxmox = {
-      source = "Telmate/proxmox"
-      version = ">= 2.9.10"
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "tnwks-ops"
+    #look for PII leak here
+    workspaces {
+      name = "tnwks-pve-prod"
     }
   }
+    required_version = ">= 1.2.2"
+   required_providers {
+     proxmox = {
+       source = "Telmate/proxmox"
+       version = ">= 2.9.10"
+   }
+     sops = {
+       source  = "carlpett/sops"
+       version = "0.7.2"
+   }
+ }
+}
+
+data "sops_file" "secrets" {
+  source_file = "secrets.sops.yaml"
 }
