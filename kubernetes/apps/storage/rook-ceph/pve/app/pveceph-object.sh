09080139ccb793fcf9001f2a85311e0a3478edb5
iT3E <26583558+iT3E@users.noreply.github.com>
2023-07-26 17:01:01 -0700

    feat: define specifics for rgw

--- kubernetes/apps/storage/rook-ceph/pve/app/pveceph-object.sh
+++ kubernetes/apps/storage/rook-ceph/pve/app/pveceph-object.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-pveceph pool create ".rgw.root" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 4
-pveceph pool create "${CLUSTER_NAME}.rgw.control" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32
-pveceph pool create "${CLUSTER_NAME}.rgw.meta" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32
-pveceph pool create "${CLUSTER_NAME}.rgw.log" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 4
-pveceph pool create "${CLUSTER_NAME}.rgw.otp" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 4
-pveceph pool create "${CLUSTER_NAME}.rgw.buckets.index" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32
-pveceph pool create "${CLUSTER_NAME}.rgw.buckets.non-ec" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32
-pveceph pool create "${CLUSTER_NAME}.rgw.buckets.data" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 64
+pveceph pool create ".rgw.root" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 4 --crush_rule hdd-hostfd
+pveceph pool create "sce-pvecl01.rgw.control" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32 --crush_rule hdd-hostfd
+pveceph pool create "sce-pvecl01.rgw.meta" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32 --crush_rule hdd-hostfd
+pveceph pool create "sce-pvecl01.rgw.log" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 4 --crush_rule hdd-hostfd
+pveceph pool create "sce-pvecl01.rgw.otp" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 4 --crush_rule hdd-hostfd
+pveceph pool create "sce-pvecl01.rgw.buckets.index" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32 --crush_rule hdd-hostfd
+pveceph pool create "sce-pvecl01.rgw.buckets.non-ec" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 32 --crush_rule hdd-hostfd
+pveceph pool create "sce-pvecl01.rgw.buckets.data" --add_storages 0 --application rgw --size 3 --min_size 2 --pg_autoscale_mode on --pg_num 64 --crush_rule hdd-hostfd
