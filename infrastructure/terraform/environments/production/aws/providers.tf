d4decdb7f990f6f411ff32c6c22f259dec6f61f7
iT3E <26583558+iT3E@users.noreply.github.com>
2023-09-03 14:25:44 -0700

    bug: tf apply for new aws account #319

--- infrastructure/terraform/environments/production/aws/providers.tf
+++ infrastructure/terraform/environments/production/aws/providers.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  access_key                  = data.sops_file.secrets.data["aws_access_key"]
-  secret_key                  = data.sops_file.secrets.data["aws_secret_key"]
+  profile                     = "sso"
   region                      = data.sops_file.secrets.data["aws_region"]
 }
