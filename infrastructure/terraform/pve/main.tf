b799e735a12e6a081347eb9186b758f07e874272
iT3E <26583558+iT3E@users.noreply.github.com>
2024-01-20 12:04:32 -0800

    👌 IMPROVE: Remove biris VM

--- infrastructure/terraform/pve/main.tf
+++ infrastructure/terraform/pve/main.tf
@@ -7,17 +7,17 @@ terraform {
       name = "tnwks-pve-prod"
     }
   }
-    required_version = ">= 1.2.2"
-   required_providers {
-     proxmox = {
-       source = "Telmate/proxmox"
-       version = "= 2.9.14"
-   }
-     sops = {
-       source  = "carlpett/sops"
-       version = "1.0.0"
-   }
- }
+  required_version = ">= 1.2.2"
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "= 2.9.14"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "1.0.0"
+    }
+  }
 }
 
 data "sops_file" "secrets" {
@@ -79,42 +79,42 @@ locals {
 #######################################
 
 module "pve_vm_k8s_masters" {
-  for_each             = toset(local.k8s_masters)
-  source               = "../../../modules/proxmox"
-  vm_name              = each.value
-  target_node          = "sce-pve0${index(local.k8s_masters, each.value) + 1}"
-  clone                = "ubuntu-server-focal"
-  full_clone           = true
-  ha_group             = "ha_group${index(local.k8s_masters, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
-  ha_state             = "started"
-  vm_memory            = "8192"
-  num_cores            = "2"
-  num_sockets          = "2"
-  qemu_os              = "l26"
-  qemu_guest_agent     = 1
-  scsihw               = "virtio-scsi-single"
-  secondary_nic_ip     = "${cidrhost("10.15.15.0/24", index(local.k8s_masters, each.value) + 100)}/24"
-  vm_nics   = [
+  for_each         = toset(local.k8s_masters)
+  source           = "../../../modules/proxmox"
+  vm_name          = each.value
+  target_node      = "sce-pve0${index(local.k8s_masters, each.value) + 1}"
+  clone            = "ubuntu-server-focal"
+  full_clone       = true
+  ha_group         = "ha_group${index(local.k8s_masters, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
+  ha_state         = "started"
+  vm_memory        = "8192"
+  num_cores        = "2"
+  num_sockets      = "2"
+  qemu_os          = "l26"
+  qemu_guest_agent = 1
+  scsihw           = "virtio-scsi-single"
+  secondary_nic_ip = "${cidrhost("10.15.15.0/24", index(local.k8s_masters, each.value) + 100)}/24"
+  vm_nics = [
     {
-      model        = "virtio"
-      bridge       = "vmbr0"
-      tag          = "120"
-      mtu          = "0"
+      model  = "virtio"
+      bridge = "vmbr0"
+      tag    = "120"
+      mtu    = "0"
     },
     {
-      model        = "virtio"
-      bridge       = "vmbr1"
-      mtu          = "9000"
+      model  = "virtio"
+      bridge = "vmbr1"
+      mtu    = "9000"
     },
   ]
   vm_disks = [
     {
-      storage   = "ssd-pool"
-      size      = "40G"
-      type      = "virtio"
-      cache     = "none"
-      format    = "raw"
-      iothread  = 1
+      storage  = "ssd-pool"
+      size     = "40G"
+      type     = "virtio"
+      cache    = "none"
+      format   = "raw"
+      iothread = 1
     },
   ]
 }
@@ -128,43 +128,43 @@ module "pve_vm_k8s_masters" {
 
 
 module "pve_vm_k8s_workers" {
-  for_each             = toset(local.k8s_workers)
-  source               = "../../../modules/proxmox"
-  vm_name              = each.value
-  target_node          = "sce-pve0${index(local.k8s_workers, each.value) + 1}"
-  clone                = "ubuntu-server-focal"
-  full_clone           = true
-  ha_group             = "ha_group${index(local.k8s_workers, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
-  ha_state             = "started"
-  vm_memory            = "24576"
-  num_cores            = "16"
-  num_sockets          = "2"
-  qemu_os              = "l26"
-  qemu_guest_agent     = 1
-  scsihw               = "virtio-scsi-single"
-  secondary_nic_ip     = "${cidrhost("10.15.15.0/24", index(local.k8s_workers, each.value) + 150)}/24"
+  for_each         = toset(local.k8s_workers)
+  source           = "../../../modules/proxmox"
+  vm_name          = each.value
+  target_node      = "sce-pve0${index(local.k8s_workers, each.value) + 1}"
+  clone            = "ubuntu-server-focal"
+  full_clone       = true
+  ha_group         = "ha_group${index(local.k8s_workers, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
+  ha_state         = "started"
+  vm_memory        = "24576"
+  num_cores        = "16"
+  num_sockets      = "2"
+  qemu_os          = "l26"
+  qemu_guest_agent = 1
+  scsihw           = "virtio-scsi-single"
+  secondary_nic_ip = "${cidrhost("10.15.15.0/24", index(local.k8s_workers, each.value) + 150)}/24"
 
-  vm_nics   = [
+  vm_nics = [
     {
-      model        = "virtio"
-      bridge       = "vmbr0"
-      tag          = "120"
-      mtu          = "0"
+      model  = "virtio"
+      bridge = "vmbr0"
+      tag    = "120"
+      mtu    = "0"
     },
     {
-      model        = "virtio"
-      bridge       = "vmbr1"
-      mtu          = "9000"
+      model  = "virtio"
+      bridge = "vmbr1"
+      mtu    = "9000"
     },
   ]
   vm_disks = [
     {
-      storage   = "ssd-pool"
-      size      = "40G"
-      type      = "virtio"
-      cache     = "none"
-      format    = "raw"
-      iothread  = 1
+      storage  = "ssd-pool"
+      size     = "40G"
+      type     = "virtio"
+      cache    = "none"
+      format   = "raw"
+      iothread = 1
     },
   ]
 }
@@ -176,35 +176,35 @@ module "pve_vm_k8s_workers" {
 #######################################
 
 module "pve_vm_vyos" {
-  for_each             = toset(local.vyos)
-  source               = "../../../modules/proxmox"
-  vm_name              = each.value
-  target_node          = "sce-pve0${index(local.vyos, each.value) + 1}"
-  clone                = "VyOS"
-  full_clone           = true
-  ha_group             = "ha_group${index(local.vyos, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
-  ha_state             = "started"
-  vm_memory            = "4096"
-  num_cores            = "2"
-  num_sockets          = "2"
-  qemu_os              = "l26"
-  qemu_guest_agent     = 1
-  scsihw               = "virtio-scsi-single"
-  vm_nics   = [
+  for_each         = toset(local.vyos)
+  source           = "../../../modules/proxmox"
+  vm_name          = each.value
+  target_node      = "sce-pve0${index(local.vyos, each.value) + 1}"
+  clone            = "VyOS"
+  full_clone       = true
+  ha_group         = "ha_group${index(local.vyos, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
+  ha_state         = "started"
+  vm_memory        = "4096"
+  num_cores        = "2"
+  num_sockets      = "2"
+  qemu_os          = "l26"
+  qemu_guest_agent = 1
+  scsihw           = "virtio-scsi-single"
+  vm_nics = [
     {
-      model        = "virtio"
-      bridge       = "vmbr0"
-      mtu          = "0"
+      model  = "virtio"
+      bridge = "vmbr0"
+      mtu    = "0"
     },
   ]
   vm_disks = [
     {
-      storage   = "ssd-pool"
-      size      = "10G"
-      type      = "virtio"
-      cache     = "none"
-      format    = "raw"
-      iothread  = 1
+      storage  = "ssd-pool"
+      size     = "10G"
+      type     = "virtio"
+      cache    = "none"
+      format   = "raw"
+      iothread = 1
     },
   ]
 }
@@ -216,77 +216,36 @@ module "pve_vm_vyos" {
 #######################################
 
 module "pve_vm_ad" {
-  for_each             = toset(local.ad)
-  source               = "../../../modules/proxmox"
-  vm_name              = each.value
-  target_node          = "sce-pve0${index(local.ad, each.value) + 1}"
-  clone                = "ubuntu-server-focal"
-  full_clone           = true
-  ha_group             = "ha_group${index(local.ad, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
-  ha_state             = "started"
-  vm_memory            = "2048"
-  num_cores            = "1"
-  num_sockets          = "2"
-  qemu_os              = "l26"
-  qemu_guest_agent     = 1
-  scsihw               = "virtio-scsi-single"
-  vm_nics   = [
+  for_each         = toset(local.ad)
+  source           = "../../../modules/proxmox"
+  vm_name          = each.value
+  target_node      = "sce-pve0${index(local.ad, each.value) + 1}"
+  clone            = "ubuntu-server-focal"
+  full_clone       = true
+  ha_group         = "ha_group${index(local.ad, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
+  ha_state         = "started"
+  vm_memory        = "2048"
+  num_cores        = "1"
+  num_sockets      = "2"
+  qemu_os          = "l26"
+  qemu_guest_agent = 1
+  scsihw           = "virtio-scsi-single"
+  vm_nics = [
     {
-      model        = "virtio"
-      bridge       = "vmbr0"
-      tag          = "110"
-      mtu          = "0"
+      model  = "virtio"
+      bridge = "vmbr0"
+      tag    = "110"
+      mtu    = "0"
     },
   ]
   vm_disks = [
     {
-      storage   = "ssd-pool"
-      size      = "40G"
-      type      = "virtio"
-      cache     = "none"
-      format    = "raw"
-      iothread  = 1
-    },
-  ]
-}
-
-#######################################
-##                                   ##
-##        PVE VM - win-biris         ##
-##                                   ##
-#######################################
-
-module "pve_vm_biris" {
-  for_each             = toset(local.biris)
-  source               = "../../../modules/proxmox"
-  vm_name              = each.value
-  target_node          = "sce-pve0${index(local.biris, each.value) + 1}"
-  clone                = "ubuntu-server-focal"
-  full_clone           = true
-  ha_group             = "ha_group${index(local.biris, each.value) + 1}" #ha groups must be pre-created in pve, with correct naming scheme, along with each having 'priorities' mapped to individual physical hosts
-  ha_state             = "started"
-  vm_memory            = "8192"
-  num_cores            = "4"
-  num_sockets          = "2"
-  qemu_os              = "l26"
-  qemu_guest_agent     = 1
-  scsihw               = "virtio-scsi-single"
-  vm_nics   = [
-    {
-      model        = "virtio"
-      bridge       = "vmbr0"
-      tag          = "610"
-      mtu          = "0"
-    },
-  ]
-  vm_disks = [
-    {
-      storage   = "ssd-pool"
-      size      = "40G"
-      type      = "virtio"
-      cache     = "none"
-      format    = "raw"
-      iothread  = 1
+      storage  = "ssd-pool"
+      size     = "40G"
+      type     = "virtio"
+      cache    = "none"
+      format   = "raw"
+      iothread = 1
     },
   ]
 }
